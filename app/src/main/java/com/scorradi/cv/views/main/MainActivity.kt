47c219f2bf950df590cd16923a4d33bd657c26ca
Sebastian Corradi <sebastiancorradi@gmail.com>
2021-03-17 01:39:20 -0300

    RX example

--- app/src/main/java/com/scorradi/cv/views/main/MainActivity.kt
+++ app/src/main/java/com/scorradi/cv/views/main/MainActivity.kt
@@ -1,21 +1,132 @@
 package com.scorradi.cv.views.main
 
+<<<<<<< Updated upstream
 import android.app.Activity
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
 import butterknife.BindView
 import butterknife.ButterKnife
 import com.scorradi.cv.R
+=======
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.scorradi.cv.databinding.ActivityMainBinding
+import com.scorradi.cv.views.components.ExperienceItemAdapter
+import com.scorradi.cv.views.fragments.JobFragment
+import com.scorradi.cv.views.models.ExperienceModel
+import com.scorradi.cv.views.models.JobModel
+import com.scorradi.cv.views.models.PersonModel
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
+>>>>>>> Stashed changes
 
 class MainActivity : Activity() {
 
     @BindView(R.id.viewPager)
     private lateinit var ViewPager: ViewPager
 
+    private lateinit var disposable: Disposable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+<<<<<<< Updated upstream
         setContentView(R.layout.activity_main)
         ButterKnife.bind(this);
+=======
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val linearLayoutManager = LinearLayoutManager(this)
+        binding.rvExperiences.layoutManager = linearLayoutManager
+
+        presenter = MainPresenter(this)
+
+        val personObservable = Observable.create<PersonModel>{
+            emitter ->
+            emitter.onNext(presenter.loadPersonModel())
+            emitter.onComplete()
+            //showPerson(personModel = presenter.loadPersonModel())
+        }
+
+        val personConsumer = object : Consumer<PersonModel> {
+            override fun accept(personModel: PersonModel?) {
+                showPerson(personModel!!)
+            }
+        }
+
+        with(personObservable) {
+            subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(personConsumer)
+        }
+
+        val experiencesObservable = Observable.create<List<ExperienceModel>>{
+                emitter ->
+            emitter.onNext(presenter.loadExperienceModels())
+            emitter.onComplete()
+            //showPerson(personModel = presenter.loadPersonModel())
+        }
+        val experiencesConsumer = object : Consumer<List<ExperienceModel>> {
+            override fun accept(experiences: List<ExperienceModel>) {
+                showExperiences(experiences)
+            }
+        }
+        
+        with(experiencesObservable) {
+            subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(experiencesConsumer)
+        }
+/*
+        disposable = personObservable
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { showPerson(it) },
+                { showError(it)},
+                {} )
+        */
+        //presenter.onCreate();
+    }
+
+    fun showError(error: Throwable){
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable.dispose()
+    }
+
+    override fun showPerson(personModel: PersonModel) {
+        binding.tvName.text = personModel.Name
+        binding.tvDNI.text = personModel.Id
+        binding.tvAge.text = Integer.toString(personModel.Age)
+        binding.tvPhoneNumber.text = personModel.PhoneNumber
+    }
+
+    override fun showExperiences(experiences: List<ExperienceModel>) {
+        val onClickListener = object : ExperienceItemAdapter.OnClickListener {
+            override fun onClick(experienceModel: ExperienceModel) {
+                presenter.onExperienceModelClick(experienceModel)
+            }
+        }
+
+        val adapter = ExperienceItemAdapter(experiences, onClickListener)
+        binding.rvExperiences.adapter = adapter
+        adapter.notifyDataSetChanged()
+    }
+
+    override fun getContext(): Context {
+        return this
+    }
+>>>>>>> Stashed changes
 
     }
 }
---
+++ app/src/main/java/com/scorradi/cv/views/main/MainPresenter.kt
@@ -0,0 +1,46 @@
+package com.scorradi.cv.views.main
+
+import android.util.Log
+import com.scorradi.cv.datamanager.DataManager
+import com.scorradi.cv.db.daos.entities.Experience
+import com.scorradi.cv.views.models.ExperienceModel
+import com.scorradi.cv.views.models.JobModel
+import com.scorradi.cv.views.models.PersonModel
+import kotlin.concurrent.thread
+
+class MainPresenter(private val mainView: IMainView) {
+    private val dataManager = DataManager()
+
+    public fun onCreate() {
+        showPerson()
+        showExperiences()
+    }
+
+    fun showPerson(){
+        val person = loadPersonModel()
+        mainView.showPerson(person)
+    }
+
+    fun showExperiences(){
+        val experiences = loadExperienceModels()
+        mainView.showExperiences(experiences)
+    }
+
+    fun loadPersonModel(): PersonModel{
+        val  person = dataManager.getPerson()
+        Log.e("Presenter", "Corriendo en hilo: " + Thread.currentThread().name)
+        val personModel:PersonModel = PersonModel(person);
+        return personModel;
+    }
+
+    fun loadExperienceModels():List<ExperienceModel>{
+        val experiences =  dataManager.getExperiences(mainView.getContext());
+        return experiences.map<Experience, ExperienceModel> { ExperienceModel(it) }
+    }
+
+    fun onExperienceModelClick(experiendeModel: ExperienceModel){
+        val job = dataManager.getJob(mainView.getContext(), experiendeModel.experienceId)
+        val jobModel = JobModel(job)
+        mainView.showJob(jobModel)
+    }
+}
